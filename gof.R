11c97a51eaa1582beef74b8dff2794c74649adb5
Gregor Ehrensperger <g@ehrensperger.dev>
2026-02-19 06:42:17 +0100

    feat: improve gof (limit axes and add absolute counts to console output)

--- gof.R
+++ gof.R
@@ -38,7 +38,7 @@ for (selection in selections) {
   weibullfit <- fitdist(sel_data, "weibull")
   
   pdf(file.path(output_folder, paste0("qqplot_", selection, ".pdf")))
-  qqcomp(list(expfit, gammafit, weibullfit), legendtext=c("exp", "gamma", "weibull"))
+  qqcomp(list(expfit, gammafit, weibullfit), legendtext=c("exp", "gamma", "weibull"), xlab="Theoretical Quantiles [mm]", ylab="Empirical Quantiles [mm]", main=paste("QQ Plot", selection), xlim=c(0, 100), ylim=c(0, 100))
   dev.off()
   
   sink(file.path(output_folder, paste0("gofstats_", selection, ".txt")))
@@ -73,3 +73,17 @@ for (selection in selections) {
   plot(weibullfit)
   dev.off()
 }
+
+#%% Plot histogram of orig data
+selection <- selections[1]
+sel_data <- getElement(sel_data_vec, selection)
+pdf(file.path(output_folder, paste0("histogram_", selection, ".pdf")))
+hist(sel_data, main=paste("Histogram of", selection), xlab="Value", ylab="Frequency", breaks=30)
+dev.off()
+
+# Bin the data into width of 10 and output counts
+bin_width <- 10
+breaks <- seq(0, ceiling(max(sel_data) / bin_width) * bin_width, by=bin_width)
+binned <- cut(sel_data, breaks=breaks, right=FALSE)
+counts <- table(binned)
+print(counts)
